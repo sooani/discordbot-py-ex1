ccf3ea256a97c98b87c90ed7f156d88ef85713a2
sooani <118452650+sooani@users.noreply.github.com>
2022-12-25 01:20:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,15 +16,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.author.client: return None
+    await client.process_commands(message)
+    
     if message.author == client.user:
         return
+    
+    if message.cotent == '@everyone':
+        await messge.channel.send('벨튀당!')
 
     if message.content == f'{PREFIX}call':
         await message.channel.send("callback!")
 
     if message.content.startswith(f'{PREFIX}hello'):
         await message.channel.send('Hello!')
-
+        
+@client.command()
+async def f'{PREFIX}안녕(ctx):
+    await ctx.channel.send(f'{ctx.message.author.mention}메로,하이메로', reference=ctx.message)
 
 try:
     client.run(TOKEN)
